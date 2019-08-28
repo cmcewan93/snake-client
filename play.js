a27d8cecda56ce158e5ef08d78f1ff823a0ca387
Colin McEwan <colin-mcewan@hotmail.com>
2019-08-28 17:37:22 +0000

    created functions to handle the user inputer

--- play.js
+++ play.js
@@ -3,3 +3,20 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect()
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = ('data', (data) => {
+  if (data === "\u0003") { //checks ctrl + c to exit
+    process.exit();
+  }
+})
